c5ade1b945ba1060e78722f8229b3624ac271e26
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-03-15 18:52:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
+const express = require("express");
+
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+const MongoDBStore = mongodbStore(session);
+const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://127.0.0.1:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+app.use(async function (req, res, next) {
+  const user = req.session.user;
+  const isAuth = req.session.isAuthenticated;
+
+  if (!user || !isAuth) {
+    return next();
+  }
+  const userDoc = await db.getDb().collection("user").findOne({ _id: user.id });
+  res.locals.isAuth = isAuth;
+  next();
+});
+app.use(demoRoutes);
+
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
